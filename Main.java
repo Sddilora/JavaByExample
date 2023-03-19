f4b616f856f0201e9abe7536a772ef61025c8e71
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-19 23:54:30 +0300

    typo fixed

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello Java"); //System(I guess package in go): Class; out:stream; println:method
+        System.out.println("Hello Java"); //System: Class(I guess same as "package" in go); out:stream; println:method
     }
 }
 
