7b51d8cec39f6a14dba18b8da844c41d316684d9
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-23 22:38:01 +0300

    int type basic example

---
+++ variables/Main.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sayi =10;
+        
+        System.out.println("Öğrenci sayım : 9 "); //System: Class(I guess same as "package" in go); out:stream; println:method
+        System.out.println(sayi);
+    }
+    
+}
