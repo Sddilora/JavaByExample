8d2703013c82f3d85876619cc3ae73d435f0c9f2
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-29 00:51:58 +0300

    Java's primitive data types exemplified

---
+++ data_types/Main.java
@@ -0,0 +1,28 @@
+package data_types;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int sayi =12;
+        sayi= 1111111111;
+        sayi=2147483647;
+
+        System.out.println(sayi);
+
+       //float sayifl =56; ??
+        double sayidbl = 10.0;
+
+        System.out.println(sayidbl);
+
+        char character = 'A'; //char holds one character
+
+        System.out.println(character);
+
+        boolean isTrue = true;
+        isTrue = false;
+
+        System.out.println(isTrue);
+
+
+    }
+}
