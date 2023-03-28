b00e66545ad03083afe44ba3c703d9a0f6e2a882
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-28 22:57:59 +0300

    string and int variables different usage

--- variables/Main.java
+++ variables/Main.java
@@ -8,6 +8,14 @@ public class Main {
         
         System.out.println("Öğrenci sayım : 9 "); //System: Class(I guess same as "package" in go); out:stream; println:method
         System.out.println(sayi);
+
+        int ogrenciSayisi =12;
+        System.out.println("Öğrenci Sayım : " + ogrenciSayisi);
+
+        String message = "Öğrenci Sayısı : ";
+        System.out.println(message+ogrenciSayisi);
+
+        
     }
     
 }
